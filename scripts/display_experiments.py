2f129e26ae3d1d53ec098a9f8ceca1b4fec6dd5a
Meelis Kull <meelis.kull@gmail.com>
2017-05-06 21:16:01 +0100

    data/mr_k_long_stay/location_annotations_to_mongo/2_create_json_records.py can now split annotations into multiple experiments; plugins/sphere/tools/annotation_state_location/2017-05-06_v0.1.1.py produces annotations as lists instead of sets, to support storing into mongodb

--- scripts/display_experiments.py
+++ scripts/display_experiments.py
@@ -23,6 +23,7 @@ from __future__ import print_function
 import arrow
 import logging
 import pandas as pd
+import pytz
 
 
 def run(house, delete_existing_workflows=True, loglevel=logging.INFO):
@@ -71,9 +72,12 @@ def run(house, delete_existing_workflows=True, loglevel=logging.INFO):
     df = M.find_stream(name='experiments_dataframe', house=house).window().values()[0]
 
     if len(df) > 0:
-        # arrow.get(x).humanize()
-        # df['start'] = df['start'].map('{:%Y-%m-%d %H:%M:%S}'.format)
+        # TODO: the following line gave a timezone error in some circumstances (when only 1 experiment?)
         df['duration'] = df['end'] - df['start']
+        # TODO: the following 3 lines worked in that cas but give an error in some cases (when more than 1 experiment?)
+        # df['start_utc'] = pd.Series(pd.DatetimeIndex(df['start']).tz_convert('UTC'))
+        # df['end_utc'] = pd.Series(pd.DatetimeIndex(df['end']).tz_convert('UTC'))
+        # df['duration'] = df['end_utc'] - df['start_utc']
         df['start'] = map(lambda x: '{:%Y-%m-%d %H:%M:%S}'.format(x), df['start'])
         df['end'] = map(lambda x: '{:%Y-%m-%d %H:%M:%S}'.format(x), df['end'])
         # df['duration'] = map(lambda x:'{:%Mmin %Ssec}'.format(x),df['duration'])
