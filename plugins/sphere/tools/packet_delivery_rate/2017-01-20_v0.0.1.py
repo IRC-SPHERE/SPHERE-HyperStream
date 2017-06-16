fe1ac8cf7d0a6345d8005c12d719d6365787bc01
Tom Diethe <tdiethe@gmail.com>
2017-06-16 14:14:44 +0100

    Commented running tests in packet delivery rate tool

--- plugins/sphere/tools/packet_delivery_rate/2017-01-20_v0.0.1.py
+++ plugins/sphere/tools/packet_delivery_rate/2017-01-20_v0.0.1.py
@@ -85,6 +85,7 @@ def test(msg, result, expected_result):
         return False
     return True
 
+
 def test_all():
     test("in order", get_pdr([11, 12, 13, 14]),  (4, 4, 1.0))
     test("mixed", get_pdr([11, 12, 14, 13]),  (4, 4, 1.0))
@@ -99,4 +100,4 @@ def test_all():
     test("with reboot two times", get_pdr([1000, 56, 5]),  (3, 3, 1.0))
     print("all tests done")
 
-test_all()
+# test_all()
